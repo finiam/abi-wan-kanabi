9605c7895eff647bb33cc14a819080eb82aebb54
David Lange <lange@finiam.com>
2023-04-03 10:29:21 +0100

    add contract w types implementation

--- index.ts
+++ index.ts
@@ -1,6 +1,33 @@
-import {Abi, ExtractAbiFunctionName, FunctionArgs, FunctionRet} from './kanabi';
+import { AccountInterface, Contract, ProviderInterface } from "starknet";
+import {
+  Abi,
+  ExtractAbiFunctionName,
+  FunctionArgs,
+  FunctionRet,
+  Functions,
+} from "./kanabi";
 
-export function
-    call<TAbi extends Abi, TFunctionName extends ExtractAbiFunctionName<TAbi>>(
-        abi: TAbi, f: TFunctionName, args: FunctionArgs<TAbi, TFunctionName>):
-        FunctionRet<TAbi, TFunctionName>{throw new Error('todo')};
+export function call<
+  TAbi extends Abi,
+  TFunctionName extends ExtractAbiFunctionName<TAbi>
+>(
+  abi: TAbi,
+  f: TFunctionName,
+  args: FunctionArgs<TAbi, TFunctionName>
+): FunctionRet<TAbi, TFunctionName> {
+  throw new Error("todo");
+}
+
+export function getContract<TAbi extends Abi>({
+  abi,
+  address,
+  account,
+}: {
+  abi: TAbi;
+  address: string;
+  account?: AccountInterface | ProviderInterface | undefined;
+}) {
+  const contract = new Contract(abi as any, address, account);
+
+  return contract as Contract & Functions<TAbi>;
+}
